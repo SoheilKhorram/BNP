26134325238b3bae83b445ee06c59c44ec38e2e5
SoheilKhorram <soheil13261428@gmail.com>
2023-04-01 20:34:13 +0430

    refactoring home's codes

--- home.php
+++ home.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
-	<meta name="viewport" content="width=1280">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=1280">
     <link href="css/home.css" rel="stylesheet">
     <script src="js/jquery.js"></script>
     <script src="js/scripts.js"></script>
@@ -51,78 +51,75 @@
     </div>
 
     <main>
-        <div class="main-page-container-part-1" >
-        <img src="images/fo.png" style="width:700px;" >
-            <div class="intro-text" >
-            <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit
-            
-            </span>
-    
-            <span>
-            incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-            </span>
-    
-            <span class="button-intro" >AJWJDS</span>
-    
+        <div class="main-page-container-part-1">
+            <img src="images/fo.png" style="width:700px;">
+            <div class="intro-text">
+                <span>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                </span>
+
+                <span>
+                    incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                </span>
+
+                <span class="button-intro">AJWJDS</span>
+
             </div>
         </div>
-    
-    <div class="gallery-container" >
-    <div class="helmet-div"> 
-    <img src="images/helmet.png" > 
-            <span>
-            incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-            incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-            </span>
-    </div>
-    <div class="gallery">
-    <img src="images/sample.png" >
-    <img src="images/sample.png" >
-    <img src="images/sample.png" >
-    </div>
-    </div>
-    
-        <div class="main-page-container-part-2" >
-        
-        <div class="main-page-section-left" ></div>
-        <div class="main-page-section-right" >
-        
-        <div class="main-page-text-section" >
-        <span>ASDFG 1</span>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
-        Adipiscing diam donec adipiscing tristique risus nec feugiat in. Viverra tellus in hac 
-        habitasse platea dictumst vestibulum. Elementum tempus egestas sed sed. Velit euismod in
+
+        <div class="gallery-container">
+            <div class="helmet-div">
+                <img src="images/helmet.png">
+                <span>
+                    incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                </span>
+            </div>
+            <div class="gallery">
+                <img src="images/sample.png">
+                <img src="images/sample.png">
+                <img src="images/sample.png">
+            </div>
         </div>
-    
-    
-        <div class="main-page-text-section" >
-        <span>ASDFG 2</span>
-        pellentesque massa placerat duis ultricies lacus. Tempus egestas sed sed risus. Urna condimentum 
-        mattis pellentesque id nibh. Sed faucibus turpis in eu mi bibendum neque egestas congue. 
-        Morbi quis commodo odio aenean sed. Amet facilisis magna etiam tempor orci eu. Quis auctor 
-        elit sed vulputate. A diam sollicitudin tempor id eu nisl nunc.
+
+        <div class="main-page-container-part-2">
+
+            <div class="main-page-text-section">
+                <span>ASDFG 1</span>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
+                    Adipiscing diam donec adipiscing tristique risus nec feugiat in. Viverra tellus in hac
+                    habitasse platea dictumst vestibulum. Elementum tempus egestas sed sed. Velit euismod in
+                </p>
+            </div>
+
+            <div class="main-page-text-section">
+                <span>ASDFG 2</span>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras adipiscing enim.
+                    Adipiscing diam donec adipiscing tristique risus nec feugiat in. Viverra tellus in hac
+                    habitasse platea dictumst vestibulum. Elementum tempus egestas sed sed. Velit euismod in
+                </p>
+            </div>
+
         </div>
-    
-        </div>
-        
-        </div>
-    
-    
-        <div class="main-page-container-part-3" >
-        <span class="part-3-title" >GFDSA</span>
-        <span class="part-3-desc" >Amet facilisis magna etiam tempor orci eu.</span>
-        
-        <div class="media-section" >
-        <div><img src="images/sample.jpg"></div>
-        <div><img src="images/sample.jpg"><img src="images/sample.jpg"></div>
-        <div style="background:#EEEEEE;width:1000px;height:100px;border-radius:10px;margin:10px 0 30px 0;" ></div>
-        </div>
-        
+
+        <div class="main-page-container-part-3">
+            <span class="part-3-title">GFDSA</span>
+            <span class="part-3-desc">Amet facilisis magna etiam tempor orci eu.</span>
+
+            <div class="media-section">
+                <div><img src="images/sample.jpg"></div>
+                <div><img src="images/sample.jpg"><img src="images/sample.jpg"></div>
+                <div style="background:var(--primary-clr-800);width:1000px;height:100px;border-radius:10px;margin:10px 0 30px 0;">
+                </div>
+            </div>
+
         </div>
     </main>
 
@@ -170,7 +167,7 @@
                     Did you know that we also operate on other platforms?
                 <p>
                     Follow us on the following platforms
-                <div class="social-media-icons" style="margin-top:20px;" >
+                <div class="social-media-icons" style="margin-top:20px;">
                     <a href="https://t.me/F14KChannel">
                         <img src="images/telegram-accent.png" alt="telegram" class="social-media-icon">
                     </a>
@@ -182,7 +179,7 @@
                         <img src="images/youtube-accent.png" alt="youtube" class="social-media-icon">
                     </a>
 
-  
+
 
                 </div>
                 <div class="social-media-icons">
@@ -192,39 +189,24 @@
                     <a href="https://twitter.com/F1_4k">
                         <img src="images/twitter-accent.png" alt="twitter" class="social-media-icon">
                     </a>
-
-        
- 
-
-
                 </div>
 
                 <div class="social-media-icons">
- 
-
-              <a href="https://discord.gg/RvNa9rQT">
+                    <a href="https://discord.gg/RvNa9rQT">
                         <img src="images/discord.png" alt="twitter" class="social-media-icon">
                     </a>
- 
-
-
                 </div>
 
-
-
-
-                </p>
-                </p>
             </div>
         </div>
     </footer>
 
     <script>
-    $(".menu-item").hover(function() {
-        $(".submenu-container", this).fadeIn(200);
-    }, function() {
-        $(".submenu-container", this).fadeOut(200);
-    });
+        $(".menu-item").hover(function() {
+            $(".submenu-container", this).fadeIn(200);
+        }, function() {
+            $(".submenu-container", this).fadeOut(200);
+        });
     </script>
 
 
