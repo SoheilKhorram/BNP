56d12cc1b997a7aebb74b98728af0bb12a6fd65e
SoheilKhorram <soheil13261428@gmail.com>
2023-04-05 19:16:21 +0430

    some basic styling to the sidebar

--- sidebar.php
+++ sidebar.php
@@ -5,10 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/sidebar.css" rel="stylesheet" type="text/css">
     <title>Sidebar</title>
 </head>
 
 <body>
+
+    <div class="sidebar">
+        <h2>BNP</h2>
+        <li class="sidebar__item">Home</li>
+        <li class="sidebar__item">User Settings</li>
+        <li class="sidebar__item">Coins</li>
+        <li class="sidebar__item">Pro User</li>
+    </div>
 
 </body>
 
